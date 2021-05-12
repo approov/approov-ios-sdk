58264aa11493fdb79d1540ddae457044c2a1b7d3
ivol <ivol@criticalblue.com>
2021-05-12 14:11:35 +0100

    Use url

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "Approov"
+            name: "Approov",
+            url: "https://github.com/approov/approov-ios-sdk.git"
             ),
     ]
 )
