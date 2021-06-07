3b4c534ddadbcdd22863fb8a96215858931956cd
ivol <ivol@criticalblue.com>
2021-06-07 07:49:19 +0100

    Update swift package version to  2.7.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "Approov",
             url: "https://github.com/approov/approov-ios-sdk/releases/download/" + releaseTAG + "/Approov.xcframework.zip",
-            checksum : "68ed56b4cc929def38dd7e00a7ce6e3f9e8ddbf5508d22b63b4b42e222deed4f"
+            checksum : "37b642944b8ce8402b90e26f20abf181d7fc7ae67ae26d464db0b6d931f97321"
             ),
     ]
 )
