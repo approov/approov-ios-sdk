b72e3ec5ea9b2b173343639cad8482910b8ee68d
ivol <ivol@criticalblue.com>
2021-06-08 15:07:27 +0100

    Update swift package version to  2.7.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let releaseTAG = "2.6.1"
+let releaseTAG = "2.7.0"
 let package = Package(
     name: "Approov",
     platforms: [.iOS(.v10)],
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "Approov",
             url: "https://github.com/approov/approov-ios-sdk/releases/download/" + releaseTAG + "/Approov.xcframework.zip",
-            checksum : "c5f80a501608a16c78727b4af7edf7f10add297c85b20e4292a8dc5a447d5f8b"
+            checksum : "7e6c67959b72d8cf2b9c2805a67e12f1448db6be9f57ec73001265c575e9a81d"
             ),
     ]
 )
