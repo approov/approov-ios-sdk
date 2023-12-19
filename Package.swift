76eb4db502dd9cdb86c465b84b7f2f1b2ecbb060
Richard Taylor <richard.taylor@criticalblue.com>
2023-12-19 16:49:40 +0000

    Don't use signed suffix

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let releaseTAG = "3.2.0-signed"
+let releaseTAG = "3.2.0"
 let package = Package(
     name: "Approov",
     platforms: [.iOS(.v12)],
